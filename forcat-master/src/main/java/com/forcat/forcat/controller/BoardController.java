f878b450c7d80e538b6d4f3dfe67bd9f4bfaa7d4
lmg <ckyun6782@naver.com>
2023-09-22 17:52:10 +0900

    09/22  p477

---
+++ forcat-master/src/main/java/com/forcat/forcat/controller/BoardController.java
@@ -0,0 +1,37 @@
+package com.forcat.forcat.controller;
+
+import com.forcat.forcat.dto.BoardDTO;
+import com.forcat.forcat.dto.PageRequestDTO;
+import com.forcat.forcat.dto.PageResponseDTO;
+import com.forcat.forcat.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
+
+
+
+    }
+
+
+
+}
